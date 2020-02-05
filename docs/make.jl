440a8d3d937234e36ffaab4adceb9adc8864bffc
Fabian Gans <fgans@bgc-jena.mpg.de>
2020-02-05 14:30:52 +0100

    include toc

--- docs/make.jl
+++ docs/make.jl
@@ -8,9 +8,9 @@ makedocs(
     #"Home" => "index.md",
     "Case studies" => [
       "Seasonality" => "ESDL case study 1 seasonality.md",
-      #"Intrinsic Dimensions" => "ESDL case study 2 Intrinsic dimension.md",
-      #"Q10" => "ESDL case study 3 q10.md",
-      #"Polygons" => "ESDL case study 4 Polygons.md",
+      "Intrinsic Dimensions" => "ESDL case study 2 Intrinsic dimension.md",
+      "Q10" => "ESDL case study 3 q10.md",
+      "Polygons" => "ESDL case study 4 Polygons.md",
       ]
   ],
   workdir = @__DIR__
